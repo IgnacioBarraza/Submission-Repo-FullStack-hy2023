91200f385d2882e6823fb43bef23d2ccc7421660
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2023-12-28 14:31:25 -0300

    feat: :sparkles: 2.8 - phonebook step 3

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -4,13 +4,23 @@ import './App.css'
 
 function App() {
   const [persons, setPersons] = useState([
-    {name: 'Arthur King'}
+    {
+      name: 'Arthur King',
+      number: 56968838945
+    },
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const onSubmit = (event) => {
     event.preventDefault();
     if (newName === '') return;
+    if (newNumber === '') return;
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      setNewName('')
+      return;
+    } 
     if (persons.some(person => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       setNewName('')
@@ -28,12 +38,14 @@ function App() {
         <form onSubmit={onSubmit}>
           <label htmlFor="name">Name: </label>
           <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)} id='name'/>
+          <label htmlFor='number'>Number: </label>
+          <input type='number' value={newNumber} onChange={(e) => setNewNumber(e.target.value)} id='number' />
           <button type='submit'>Add person</button>
         </form>
         <h2>Numbers</h2>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           {persons.map((person)  => {
-            return <span key={person}>{person.name}</span>
+            return <span key={person}>{person.name} - {person.number}</span>
           })}
         </div>
       </div>

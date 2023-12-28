861d7f06923d5a5b435b59af8deaa1bc42b1f6df
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2023-12-28 13:46:55 -0300

    feat: :sparkles: 2.6 - phonebook step 1

---
+++ Part2/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [persons, setPersons] = useState([
+    {name: 'Arthur King'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (newName === '') return;
+    // Add person to state array
+    setPersons([...persons, {name: newName}]);
+    setNewName(''); // clear the input field after adding a new person
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Phonebook list</h2>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="name">Name: </label>
+          <input type='text' value={newName} onChange={(e) => setNewName(e.target.value)} id='name'/>
+          <button type='submit'>Add person</button>
+        </form>
+        <h2>Numbers</h2>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          {persons.map((person)  => {
+            return <span key={person}>{person.name}</span>
+          })}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
